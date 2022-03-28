e7e3b7ce7e4aa3baf5ff958fa0bb287759517d5f
martyewings <martyewings@gmail.com>
2022-03-28 12:35:09 +0100

    first commit

--- manifests/scrape.pp
+++ manifests/scrape.pp
@@ -4,13 +4,14 @@
 #
 # @example
 #   include forge_scrape_plot::scrape
-class forge_scrape_plot::scrape(
+class forge_scrape_plot::scrape (
 
-Array[String] $modules,
-String scrape_dir  = '/var/log/forge_scrape/',
-Boolean plot = false,
-)
-  {
-
-
+  Array[String] $modules = ['puppetlabs-support_tasks', 'puppetlabs-puppet_operational_dashboards', 'puppetlabs-pe_status_check', 'puppetlabs-influxdb'],
+  String $scrape_dir  = '/var/log/forge_scrape/',
+  Boolean $plot = false,
+) {
+  file { '/var/forge_plot.sh':
+    ensure  => file,
+    content => template('forge_scrape_plot/forge_scrape.epp'),
+  }
 }
