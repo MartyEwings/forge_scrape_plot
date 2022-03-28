eb70d42fdbfdd9622a0a19db1991a3659f9f957f
martyewings <martyewings@gmail.com>
2022-03-28 12:23:42 +0100

    first commit

---
+++ manifests/scrape.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include forge_scrape_plot::scrape
+class forge_scrape_plot::scrape(
+
+Array[String] $modules,
+String scrape_dir  = '/var/log/forge_scrape/',
+Boolean plot = false,
+)
+  {
+
+
+}
